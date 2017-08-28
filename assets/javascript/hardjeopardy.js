02d00cc98f5addbcdd69458af492e91dfb5698c3
Rudy Morosoff <rudy.morosoff@libertymutual.com>
2017-08-27 17:26:39 -0700

    Added code for hard mode in hardindex.html and hardjeopardy.js

--- assets/javascript/hardjeopardy.js
+++ assets/javascript/hardjeopardy.js
@@ -1,92 +1,198 @@
-
-(function(){
+(function() {
 
   //jQuery equivelent to window.onload = function{}
   //code in here wont run until page loads
-  $(function(){
+  $(function() {
 
     // Buttons we need from the DOM, retrieved using jQuery
-    let questionButton = $("#get-clue");
-    let guessButton = $("#guess-button");
+    //let questionButton = $("#get-clue1");
+    //let guessButton = $("#guess-button1");
 
     //  set crowd noise objects
     let cheeringSound = new Audio("assets/images/crowdapplause.wav");
     let laughingSound = new Audio("assets/images/crowdlaughing.wav");
 
-    //  Other items we need from the DOM, using jQuery to get them
+    //  Other items we need from the DOM on subheader, using jQuery to get them
     let currentScore = $("#score");
-    let questionCategory = $("#category");
-    let questionValue = $("#value");
-    let questionText = $("#question");
-    let answerText = "";
     let questionHeader = $("#get-clue-text");
     let answerHeader = $("#guess-button-text");
 
-    /**
-     * Gets the text from the answer box
-     * @return {String}
-     */
-    function getText() {
-      return $('#save-me').val();
+    //  initialize global variables for answer text
+    let answerText1 = "";
+    let answerText2 = "";
+    let answerText3 = "";
+
+    // *******************************************************************************************
+    // **********  Functions needed for Jeopardy Game ********************************************
+    // *******************************************************************************************
+
+    // random integer generator between min and max inclusive to be used for offset
+    //  found this by googling random # 1 to 10 on MDN (Math.random)
+    function getRandomIntInclusive(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
     }
 
-    /**
-     * Puts text in the answer box to clear it out
-     * @param {String} text the stuff you want to put in the box
-     */
-    function setText(text) {
-      return $('#save-me').val(text);
-    }
+    // function to get next questions and show and hide appropriate DOM elements
+    function getQuestion() {
+      // random number to use as offset to get different categries each time
+      let offset = getRandomIntInclusive(1, 1000);
+      // set the string to send for category request
+      let categoryString = "http://jservice.io/api/categories?count=3&offset=" + offset;
+      console.log("Category string is " + categoryString);
 
-    // function to get next question and hide questionText and guess-button
-    function getQuestion(){
-      //  get question from api
-      $.get("http://jservice.io/api/random", function(data){
-        console.log(JSON.stringify(data));
-        //  set html of variables from retrieved item
-        questionText.html(data[0].question);
-        questionCategory.html(data[0].category.title);
-        questionValue.html(data[0].value);
-        answerText = data[0].answer;
-        console.log(answerText);
+      // Call jeopardy API and get 3 categories
+      $.get(categoryString, function(catArray) {
+        console.log(JSON.stringify(catArray));
+
+        //  Handle question ONE stuff
+        let questionString1 = ("http://jservice.io/api/clues?category=" + catArray[0].id);
+        console.log(questionString1);
+        //  get question1 from api
+        $.get(questionString1, function(data) {
+          console.log(JSON.stringify(data));
+          //  set html of variables from retrieved item
+          $("#question1").html(data[0].question);
+          $("#category1").html(data[0].category.title);
+          $("#value1").html(data[0].value);
+          answerText1 = data[0].answer.toUpperCase();
+          console.log("Answer one is " + answerText1);
+        })
+
+        //  Handle question TWO stuff
+        let questionString2 = ("http://jservice.io/api/clues?category=" + catArray[1].id);
+        console.log(questionString2);
+        //  get question1 from api
+        $.get(questionString2, function(data) {
+          //  set html of variables from retrieved item
+          $("#question2").html(data[0].question);
+          $("#category2").html(data[0].category.title);
+          $("#value2").html(data[0].value);
+          answerText2 = data[0].answer.toUpperCase();
+          console.log("Answer two is " + answerText2);
+        })
+
+        //  Handle question THREE stuff
+        let questionString3 = ("http://jservice.io/api/clues?category=" + catArray[2].id);
+        console.log(questionString3);
+        //  get question1 from api
+        $.get(questionString3, function(data) {
+          //  set html of variables from retrieved item
+          $("#question3").html(data[0].question);
+          $("#category3").html(data[0].category.title);
+          $("#value3").html(data[0].value);
+          answerText3 = data[0].answer.toUpperCase();
+          console.log("Answer three is " + answerText3);
+        })
+
       })
       //  hide and show items accordingly
-      questionText.hide();
-      guessButton.hide();
+      $("#question1").hide();
+      $("#guess-button1").hide();
+      $("#get-clue1").show();
+      $("#question2").hide();
+      $("#guess-button2").hide();
+      $("#get-clue2").show();
+      $("#question3").hide();
+      $("#guess-button3").hide();
+      $("#get-clue3").show();
       answerHeader.hide();
       questionHeader.show();
-      questionButton.show();
+      $("#card1").show();
+      $("#card2").show();
+      $("#card3").show();
     }
 
+
+    //********************************************************************
     //  Beginning of main flow.  Call getQuestion to load first question
+    //********************************************************************
     getQuestion();
 
-    // When the questionButton is clicked, lets show and hide appropriately
-    questionButton.click(function(){
-      questionButton.hide();
+    // When the $("#get-clue1") is clicked, lets show and hide appropriately
+    $("#get-clue1").click(function() {
+      $("#get-clue1").hide();
       questionHeader.hide();
       answerHeader.show();
-      questionText.show();
-      guessButton.show();
-      })
+      $("#question1").show();
+      $("#guess-button1").show();
+      $("#card3").hide();
+      $("#card2").hide();
+    })
 
-    //  when guessButton is clicked, do the following
-    guessButton.click(function(){
-      let guessText = getText();  // capture answer from box
+    // When the $("#get-clue2") is clicked, lets show and hide appropriately
+    $("#get-clue2").click(function() {
+      $("#get-clue2").hide();
+      questionHeader.hide();
+      answerHeader.show();
+      $("#question2").show();
+      $("#guess-button2").show();
+      $("#card1").hide();
+      $("#card3").hide();
+    })
+
+    // When the $("#get-clue3") is clicked, lets show and hide appropriately
+    $("#get-clue3").click(function() {
+      $("#get-clue3").hide();
+      questionHeader.hide();
+      answerHeader.show();
+      $("#question3").show();
+      $("#guess-button3").show();
+      $("#card1").hide();
+      $("#card2").hide();
+    })
+
+    //  when $("#guess-button1") is clicked, do the following
+    $("#guess-button1").click(function() {
+      let guessText = $('#save-me1').val().toUpperCase(); // capture answer from box
       console.log(guessText);
       //  check user guess against real answer
-      if (guessText === answerText) {
-        console.log("The current score is " + currentScore.html());
-        console.log("The question value is " + questionValue.html());
-        //  add questionValue to current score
-        currentScore.html(parseInt(currentScore.html()) + parseInt(questionValue.html()));
-        console.log("Now, the cureent score is " + currentScore.html());
+      if (guessText === answerText1) {
+        //  add $("#value1") to current score
+        currentScore.html(parseInt(currentScore.html()) + parseInt($("#value1").html()));
         cheeringSound.play();
       } else {
         laughingSound.play();
       }
       // clear out the answer box
-      setText("");
+      $('#save-me1').val("");
+      // now lets get the next question
+      getQuestion();
+    })
+
+    //  when $("#guess-button2") is clicked, do the following
+    $("#guess-button2").click(function() {
+      let guessText = $('#save-me2').val().toUpperCase(); // capture answer from box
+      console.log(guessText);
+      //  check user guess against real answer
+      if (guessText === answerText2) {
+        //  add $("#value1") to current score
+        currentScore.html(parseInt(currentScore.html()) + parseInt($("#value2").html()));
+        cheeringSound.play();
+      } else {
+        laughingSound.play();
+      }
+      // clear out the answer box
+      $('#save-me2').val("");
+      // now lets get the next question
+      getQuestion();
+    })
+
+    //  when $("#guess-button3") is clicked, do the following
+    $("#guess-button3").click(function() {
+      let guessText = $('#save-me3').val().toUpperCase(); // capture answer from box
+      console.log(guessText);
+      //  check user guess against real answer
+      if (guessText === answerText3) {
+        //  add $("#value1") to current score
+        currentScore.html(parseInt(currentScore.html()) + parseInt($("#value3").html()));
+        cheeringSound.play();
+      } else {
+        laughingSound.play();
+      }
+      // clear out the answer box
+      $('#save-me3').val("");
       // now lets get the next question
       getQuestion();
     })
